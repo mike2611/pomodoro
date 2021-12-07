f068c522d6aa990c1c12fa9f39e1c54949fafe18
mike2611 <mpuentes2611@gmail.com>
2021-12-07 17:26:01 -0600

    Add styles to Headercomponent

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
+import clockLogo from '../img/logo-timer.gif'
+
+
 const Header = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <h1 class="navbar-brand" href="#">PomoTimer</h1>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-color1">
+        <div class="navbar-brand me-3 me-md-auto d-flex align-items-center">
+            <h1 class="ms-3">PomoTimer</h1>
+            <img class="logo-timer ms-2" src={clockLogo}/>
+        </div>
+        <div class="me-3 mt-3">
             <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Timer </a>
+                <a class="nav-link" href="#"> 
+                    <h2 class="nav-text"> Timer </h2> 
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="#">
+                    <h2 class="nav-text"> About </h2> 
+                </a>
             </li>
             </ul>
         </div>
