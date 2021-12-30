a179c44b222a2ca23c3aca6b6a8f7d5abd81686c
mike2611 <mpuentes2611@gmail.com>
2021-12-29 22:13:29 -0600

    Add functionality to the timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,23 +1,45 @@
-const Timer = () => ( 
+import React, { useEffect, useState } from 'react';
+
+const Timer = () => {
+  const [timeLeft, setTimeLeft] = useState(
+    {
+      minutes: 25,
+      seconds: 0,
+    });
+
+  useEffect(() => {
+    const updateCounter = () => {
+      if( timeLeft.seconds === 0 ) {
+        return (
+          {minutes: timeLeft.minutes - 1, seconds: 59}
+        );
+      }
+
+      return (
+        {minutes: timeLeft.minutes, seconds: timeLeft.seconds - 1}
+      );
+    }
+
+    setTimeout(() => {
+      setTimeLeft(updateCounter);
+    }, 1000)
+  }, [timeLeft])
+
+  
+  return(
   <div class="section-container d-flex justify-content-center align-items-center">
     <div class="clock-container">
       <h2 class="text-white text-center mb-3">Next Short Break </h2>
       <div class="clock">
         <div class="minutes">
           <div>
-            <div>0</div>
-          </div>
-          <div>
-            <div>0</div>
+            <div>{timeLeft.minutes}</div>
           </div>
         </div>
         <div class="tick">:</div>
         <div class="seconds">
           <div>
-            <div>0</div>
-          </div>
-          <div>
-            <div>0</div>
+            <div>{timeLeft.seconds < 10 ? `0${timeLeft.seconds}` : timeLeft.seconds}</div>
           </div>
         </div>
       </div>
@@ -27,6 +49,7 @@ const Timer = () => (
       </div>
     </div>
   </div>
-);
+  );
+};
 
 export default (Timer);
