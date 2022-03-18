24eea68724d74a3f598a230633d23ac8ccc56a92
mike2611 <mpuentes2611@gmail.com>
2022-03-18 16:56:46 -0600

    Add modal for setting timer

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-const About = () => ( 
+const About = () => (
   <div className="section-container overflow-auto">
     <h1 className="text-muted mb-5 mt-1 ms-3"> The <span className="text-danger">Pomodoro®</span> Core Process consists of 6 steps: </h1>
 
@@ -8,10 +8,9 @@ const About = () => (
       <li className="text-white mb-5 ms-3 h3">Work on the task until the pomotimer rings</li>
       <li className="text-white mb-5 ms-3 h3">When the pomotimer rings, put a checkmark on a paper</li>
       <li className="text-white mb-5 ms-3 h3">Take a short break</li>
-      <li className="text-white mb-5 ms-3 h3">Every 4 <span className="text-danger">Pomodoro®</span>, take a longer break</li> 
+      <li className="text-white mb-5 ms-3 h3">Every 4 <span className="text-danger">Pomodoro®</span>, take a longer break</li>
     </ol>
   </div>
 );
 
 export default (About);
-  
--- src/components/PomoTimer.js
+++ src/components/PomoTimer.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import Modal from './Modal';
 
 const PomoTimer = () => {
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
   const [startTimer, setStartTimer] = useState(false);
   const [breakTime, setBreakTime] = useState(false);
+  const [showModal, setShowModal] = useState(true);
 
   useEffect(() => {
     if (startTimer) {
@@ -29,8 +31,6 @@ const PomoTimer = () => {
     }
   }, [seconds, startTimer]);
 
-
-
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
@@ -44,9 +44,21 @@ const PomoTimer = () => {
           <h2>{timerMinutes}:{timerSeconds}</h2>
         </div>
         <div className="d-flex justify-content-center mt-5">
-          <button className="mx-2 btn btn-warning btns-clock py-3">RESET</button>
-          <button className={startTimer ? 'mx-2 btn btn-danger btns-clock py-3' : 'mx-2 btn btn-primary btns-clock py-3'} onClick={() => (setStartTimer((prevState) => !prevState))}>{startTimer ? 'STOP' : 'START'}</button>
+          <button className={startTimer ? 'mx-2 px-3 btn btn-danger btns-clock p-3' : 'mx-2 btn btn-primary btns-clock p-3'} onClick={() => (setStartTimer((prevState) => !prevState))}>{startTimer ? 'STOP' : 'START'}</button>
         </div>
+        {
+          showModal ? (
+            <Modal>
+              <div>
+                <label htmlFor="workTime">
+                  Pomodoro (minutes)
+                  <input id="workTime" className="col-md-4 m-4" onChange={(e) => setMinutes(e.target.value)} value={minutes} placeholder="25" />
+                </label>
+                <button className="mx-2 px-3 btn btn-danger btns-clock p-3" onClick={() => setShowModal((prevState) => (!prevState))}>Set time</button>
+              </div>
+            </Modal>
+          ) : null
+        }
       </div>
     </div>
   );
