182dc28d50e99a3daeb93edcba29c5fb69ee4be9
mike2611 <mpuentes2611@gmail.com>
2022-03-17 18:58:10 -0600

    Add timer

--- src/components/PomoTimer.js
+++ src/components/PomoTimer.js
@@ -1,42 +1,66 @@
-import React, { useState } from 'react';
-import Timer from './Timer';
+import React, { useState, useRef, useEffect } from 'react';
 
 const PomoTimer = () => {
-  const [timeLeft, setTimeLeft] = useState
-  (
-    {
-      minutes: 25,
-      seconds: 0,
+  const Ref = useRef(null);
+  const [timer, setTimer] = useState('00:00:00');
+
+
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / 1000 * 60 * 60) % 24);
+    return {
+      total, hours, minutes, seconds
+    };
+  }
+
+  const startTimer = (e) => {
+    let { total, hours, minutes, seconds } = getTimeRemaining(e);
+    if (total >= 0) {
+      setTimer(
+        (hours > 9 ? hours : '0' + hours) + ':' +
+        (minutes > 9 ? minutes : '0' + minutes) + ':'
+        + (seconds > 9 ? seconds : '0' + seconds)
+      )
     }
-  );
+  }
 
-  const [start, setStart] = useState(false);
+  const clearTimer = (e) => {
+    setTimer('00:00:10');
 
-  const resetTimer = (() => {
-    setTimeLeft
-    (
-      {
-        minutes: 25,
-        seconds: 0,
-      }
-    );
-  });
+    if (Ref.current) clearInterval(Ref.current);
+    const id = setInterval(() => {
+      startTimer(e);
+    }, 1000)
+    Ref.current = id;
+  }
 
-  
-  return(
-  <div className="section-container d-flex justify-content-center align-items-center">
-    <div className="clock-container">
-      <h2 className="text-white text-center mb-3">Next Short Break </h2>
-      <div className="clock d-flex justify-content-center">
-        <Timer />
-      </div>
-      <div className="d-flex justify-content-center mt-5">
-        <button className="mx-2 btn btn-warning btns-clock py-3" onClick={resetTimer}>RESET</button>
-        <button className={start ? 'mx-2 btn btn-danger btns-clock py-3' : 'mx-2 btn btn-primary btns-clock py-3'} 
-        onClick={() => setStart((prevState) => !prevState)}>{start ? 'STOP' : 'START'}</button>
+  const getDeadTime = () => {
+    let deadline = new Date();
+
+    deadline.setSeconds(deadline.getSeconds() + 10);
+    return deadline;
+  }
+
+  const onClickReset = () => {
+    clearTimer(getDeadTime());
+  }
+
+
+  return (
+    <div className="section-container d-flex justify-content-center align-items-center">
+      <div className="clock-container">
+        <h2 className="text-white text-center mb-3">Next Short Break </h2>
+        <div className="clock d-flex justify-content-center">
+          <h2>{timer}</h2>
+        </div>
+        <div className="d-flex justify-content-center mt-5">
+          <button className="mx-2 btn btn-warning btns-clock py-3" onClick={onClickReset}>RESET</button>
+          <button className="mx-2 btn btn-primary btns-clock py-3" onClick={onClickReset}>START</button>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
